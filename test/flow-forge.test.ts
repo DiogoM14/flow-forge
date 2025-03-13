9735065e693fe7047d3c10eedc2f7b792ea25e82
Diogo Martins <diogomartins200214@gmail.com>
2025-03-13 19:15:55 +0000

    Initial commit

---
+++ test/flow-forge.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/flow-forge"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
